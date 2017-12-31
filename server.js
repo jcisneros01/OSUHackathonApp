0fc5b89cf778e8dfd3790c26a9f5bb8a6df7b154
jcisneros01 <cisnejos@oregonstate.edu>
2017-12-30 17:16:46 -0700

    changed port

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function broadcast(event, data) {
   });
 }
 
-server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
