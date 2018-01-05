ba0da5df951d1715982ab41b7ff6d4bfe67f42f7
jcisneros01 <cisnejos@oregonstate.edu>
2018-01-05 00:34:15 -0700

    added show cards route that prints to console

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
-// Set up
+// Set up 
 var express = require("express");
 var app = express();
-app.set('port', 3000); // set port
-app.use(express.static(__dirname + '/public'));
+app.set('port', 4000); // set port
+app.use(express.static(__dirname + '/public')); // static files
+app.set("view engine", "ejs");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/default_deck");
+
+// MONGOOSE/MODEL CONFIG
+var cardSchema = new mongoose.Schema({
+    front: String,
+    back: String
+});
+
+var Card = mongoose.model("Card", cardSchema);
+
+// Add test card
+Card.create({
+   front: "What is the meaning of life?",
+   back: "42"
+}, function(err, cat){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(cat);
+    }
+});
 
 // Routes
 app.get("/", function(req, res){
-  res.send("index.html");
+   res.redirect("/cards"); 
 });
 
+// INDEX ROUTE
+app.get("/cards", function(req, res){
+   Card.find({}, function(err, cards){
+       if(err){
+           console.log("ERROR!");
+       } else {
+          console.log(cards);
+       }
+   });
+});
 
 app.use(function(req,res){
   res.status(404);
