9bf0bf0037841c8f4ca98ef1a7099fc6b4f530cf
nvillegas <nvillegasx@gmail.com>
2018-01-06 20:26:35 -0800

    random mode UI and js

---
+++ public/js/randommode.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', getCards);
+
+var cards;
+var position = Math.random() * ( cards.length - 0) + cards.length;
+
+function getCards() {
+    var req = new XMLHttpRequest();
+    var url = "/cards";
+    req.open("GET", url, true);
+    req.addEventListener('load', function() {
+        if (req.status >= 200 && req.status < 400) {
+            cards = JSON.parse(req.responseText);
+            console.log(cards);
+            displayFront();
+        } else {
+            console.log("Error in network request: " + req.statusText);
+        }
+    });
+    req.send(null);
+}
+
+function displayFront() {
+    var cardView = document.getElementById("cardView");
+    cardView.innerHTML = cards[position].front;
+}
+
+function traverseArray() {
+    position = Math.random() * (cards.length - 0) + cards.length;
+    if (position < cards.length) {
+        displayFront();
+    } else {
+        //user has done the study, show him the congrat page
+        window.location.replace("/studydone");
+    }
+}
+
+function displayBack() {
+    var cardView = document.getElementById("cardView");
+    cardView.innerHTML = cards[position].back;
+}
