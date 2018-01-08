ff30458170e313fcd84d4752332d347e3bc1a8d6
seag11 <seag11@users.noreply.github.com>
2018-01-07 19:13:37 -0800

    deactivates study mode if deck is empty

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', defaultCheck);
+document.addEventListener('DOMContentLoaded', freezeLink);
 
 function updateDeck(){
 	document.getElementById("curdeck").innerText = "Current deck: " + localStorage.deck;	
@@ -11,3 +12,32 @@ function defaultCheck(){
 	updateDeck();
 }
 
+var cards;
+
+/************************************************
+ *                 freezeLink
+ * deactivates study modes if set of cards is empty 
+ ***********************************************/
+function freezeLink() {
+    var req = new XMLHttpRequest();
+    var url = "/deck/" + localStorage.deck;
+    req.open("GET", url, true);
+    req.addEventListener('load', function() {
+        if (req.status >= 200 && req.status < 400) {
+            cards = JSON.parse(req.responseText);
+			if(cards.length == 0){
+				document.getElementById("studymode").setAttribute("href","");
+				document.getElementById("studymode").setAttribute("style", "color: gray");
+				document.getElementById("randommode").setAttribute("href","");
+				document.getElementById("randommode").setAttribute("style", "color: gray");
+			}
+			else{
+				document.getElementById("studymode").setAttribute("href","/study");
+				document.getElementById("randommode").setAttribute("href","/randommode");
+			}
+        } else {
+            console.log("Error in network request: " + req.statusText);
+        }
+    });
+    req.send(null);
+}
